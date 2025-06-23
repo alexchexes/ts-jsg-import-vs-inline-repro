f88977b45996878022053aa643553f1f7e3e76da
alexchexes <alexchexes@gmail.com>
2025-06-23 21:31:51 +0100

    re-generate after the fix

--- src/main-type-fest.ts
+++ src/main-type-fest.ts
@@ -1,5 +1,8 @@
 import type { OverrideProperties as Override } from 'type-fest';
 
+/** 
+ * Some description of the interface
+ */
 interface SomeInterface {
   foo: string;
   bar: string;
@@ -7,6 +10,9 @@ interface SomeInterface {
   qux: null;
 }
 
+/** 
+ * Some description of the object
+ */
 export type MyObject = Override<
   SomeInterface,
   {
